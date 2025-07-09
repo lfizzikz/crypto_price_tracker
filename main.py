7bb990dbafd35c4cf5d6a67a1110e5e9cd0eaa8d
Trevor Nance <trevornance7@gmail.com>
2025-07-09 17:58:05 -0400

    added error handling for certain input types

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 import requests
 
-coins = input("Enter the coins you want: ")
-url_assets = f"https://rest.coincap.io/v3/assets?ids={coins}"
+aliases = {"btc": "bitcoin", "eth": "ethereum", "sol": "solana", "ltc": "lightcoin"}
+raw_input = input("Enter the coins you want: ")
+coins = []
+for coin in raw_input.lower().replace(" ", "").split(","):
+    full_name = aliases.get(coin, coin)
+    coins.append(full_name)
+
+coins_list = ",".join(coins)
+url = f"https://rest.coincap.io/v3/assets?ids={coins_list}"
 headers = {
     "accept": "application/json",
     "Authorization": "Bearer a4060ca4aed5b1e6b169cb6a7d013622abb7fb4e8fe32229d77c16db8e5d1a6b",
 }
 
-response_assets = requests.get(url_assets, headers=headers)
+response = requests.get(url, headers=headers)
 
-if response_assets.status_code == 200:
-    data_assets = response_assets.json()
+if response.status_code == 200:
+    data_assets = response.json()
     for item in data_assets["data"]:
         rate = round(float(item["priceUsd"]), 2)
         assets = round(float(item["changePercent24Hr"]), 2)
         print(f"{item["symbol"]} - ${rate:,.2f} | 24h Change: {assets}%\n")
 else:
-    print("nothing found", response_assets.status_code)
+    print("nothing found", response.status_code)
